78825b3a30777d208296e3f0e61a5c0d26ea24a9
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-04-10 14:51:42 -0700

    abstract buffers

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import './index.css';
 import standardVertSrc from './standard.vert';
 import standardFragSrc from './standard.frag';
 
-import * as shader from './shader';
-import * as quad from './quad';
+import {compileShader} from './shader';
+import {createVertBuf, createIdxBuf} from './quad';
 import {ProgramInfo} from './program';
 
 /** @type {HTMLCanvasElement} */
@@ -20,12 +20,12 @@ gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
-const standardVert = shader.compileShader(
+const standardVert = compileShader(
     gl,
     gl.VERTEX_SHADER,
     standardVertSrc,
 );
-const standardFrag = shader.compileShader(
+const standardFrag = compileShader(
     gl,
     gl.FRAGMENT_SHADER,
     standardFragSrc,
@@ -40,18 +40,11 @@ const standardProgram = new ProgramInfo(
 
 standardProgram.use(gl);
 
-const quadBuf = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, quadBuf);
-gl.bufferData(gl.ARRAY_BUFFER, quad.vertices, gl.STATIC_DRAW);
-
-const quadIdxBuf = gl.createBuffer();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, quadIdxBuf);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, quad.indices, gl.STATIC_DRAW);
-
-const quadIdxType = gl.UNSIGNED_SHORT;
+createVertBuf(gl);
+createIdxBuf(gl);
 
 const posLoc = standardProgram.attribs.aPosition;
 gl.vertexAttribPointer(posLoc, 2, gl.FLOAT, false, 0, 0);
 gl.enableVertexAttribArray(posLoc);
 
-gl.drawElements(gl.TRIANGLES, 6, quadIdxType, 0);
+gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
--- src/quad.js
+++ src/quad.js
@@ -1,11 +1,35 @@
-export const vertices = new Float32Array([
+const vertices = new Float32Array([
   1.0, 1.0, // top right
   -1.0, 1.0, // top left
   1.0, -1.0, // bottom right
   -1.0, -1.0, // bottom left
 ]);
 
-export const indices = new Uint16Array([
+/**
+ * creates and binds the vertex buffer for a quad
+ * @param {WebGLRenderingContext} ctx
+ * @return {WebGLBuffer}
+ */
+export function createVertBuf(ctx) {
+  const quadBuf = ctx.createBuffer();
+  ctx.bindBuffer(ctx.ARRAY_BUFFER, quadBuf);
+  ctx.bufferData(ctx.ARRAY_BUFFER, vertices, ctx.STATIC_DRAW);
+  return quadBuf;
+}
+
+const indices = new Uint16Array([
   0, 1, 2,
   1, 3, 2,
 ]);
+
+/**
+ * create the index buffer of a quad
+ * @param {WebGLRenderingContext} ctx
+ * @return {WebGLBuffer}
+ */
+export function createIdxBuf(ctx) {
+  const idxBuf = ctx.createBuffer();
+  ctx.bindBuffer(ctx.ELEMENT_ARRAY_BUFFER, idxBuf);
+  ctx.bufferData(ctx.ELEMENT_ARRAY_BUFFER, indices, ctx.STATIC_DRAW);
+  return idxBuf;
+}
